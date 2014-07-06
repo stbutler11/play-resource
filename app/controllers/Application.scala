e81ed5a791ee60d2e34b7be4e080344c457006fc
stbutler11 <stbutler11@hotmail.com>
2014-07-06 17:18:36 +0100

    Created ResourceController

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,29 +3,20 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.libs.json._
+import models.ResourceXDataStore
+import models.ResourceX
+import models.ResourceDataStore
 
 
-object Application extends Controller {
+object Application extends ResourceController[ResourceX] 
+		with EmberJson[ResourceX] {
   
-  implicit val resFormt = Json.format[Res]
+  implicit val resFormt = Json.format[ResourceX]
+  
+  val resourceDataStore:ResourceDataStore[ResourceX] = ResourceXDataStore
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
-  def get(id: String) =  GetResourceAction(id, lookupResource) { res =>
-  	Ok("Found resource: " + res.name)
-  }
-  
-  def create = CreateResourceAction[Res] { res =>
-      Ok("Created resource again: " + res.name)
-  }
-  
-  def update(id: String) = UpdateResourceAction[Res](id, lookupResource) { res =>
-      Ok("Created resource again: " + res.name)
-  }
-  
-  def lookupResource(id:String):Res = {
-    Res("Name_" + id);
-  }
+
 }
